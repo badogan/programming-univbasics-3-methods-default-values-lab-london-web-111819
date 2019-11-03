b21efaace38074eeae4de74b3bab17720e6a6fb0
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-03 21:17:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,6 @@
 def meal_choice(veg1,veg2,protein="meat")
    puts "What a nutritious meal!"
    return_value = "A plate of #{protein} with #{veg1} and #{veg2}." 
-   retrun return_value
+   puts return_value
+   return return_value
 end 
